490d4b002fc94c262abd8e587559e83a6586d25f
Library of nyamyumba <stenahfils@gmail.com>
2025-08-30 09:05:49 +0200

    Create App.jsx

---
+++ Src/Src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WelcomeCard from './components/WelcomeCard'
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center", padding: "40px" }}>
+      <h1>Irembo ry’Ubwenge in Nyamyumba</h1>
+      <p>The Gateway of Wisdom – Digital Library</p>
+      <WelcomeCard />
+    </div>
+  )
+}
